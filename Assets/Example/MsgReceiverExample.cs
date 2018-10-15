06460b10f7b0c4d885a04ad4b77f20108f863d5c
公司 <1413698201@qq.com>
2018-10-15 15:24:27 +0800

    gs

---
+++ Assets/Example/MsgReceiverExample.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniFramework;
+public class MsgReceiverExample : MonoBehaviour,IMsgReceiver {
+
+	// Use this for initialization
+	void Start () {
+        this.RegisterMsg("xx", (param) => Debug.Log("我接收到了:"+param[0]));
+	}
+}
---
+++ Assets/Example/MsgSenderExample.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniFramework;
+public class MsgSenderExample : MonoBehaviour,IMsgSender {
+
+	// Use this for initialization
+	void Start () {
+        this.SendMsg("xx", "hello world");
+	}
+}
