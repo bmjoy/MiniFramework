9383e7d6bc591a2715d4c7768b9cef099928fd12
apple <apple@appledeiMac.local>
2019-03-01 14:11:21 +0800

    update 2019.3.1

---
+++ Assets/MiniFramework/3.IOModule/Security/SecurityUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniFramework
+{
+    public abstract class SecurityUtil
+    {
+        public static DES DES { get { return new DES(); } }
+        public static AES AES { get { return new AES(); } }
+        public static RSA RSA { get { return new RSA(); } }
+        public static MD5 MD5 { get { return new MD5(); } }
+        public abstract string Encrypt(string normalText, string key = null);
+        public virtual string Decrypt(string encryptText, string key) { return ""; }
+    }
+}
---
+++ Assets/MiniFramework/3.IOModule/Serialize/SerializeUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniFramework
+{
+    public abstract class SerializeUtil
+    {
+        public static Binary Binary { get { return new Binary(); } }
+        public static Xml Xml { get { return new Xml(); } }
+        public static Json Json { get { return new Json(); } }
+        public static ProtoBuff ProtoBuff { get { return new ProtoBuff(); } }
+        public abstract byte[] Serialize(object obj);
+        public abstract T Deserialize<T>(byte[] data);
+    }
+}
